f441a6982aa7f6afad9d7f7009b2b10f48c5fce7
marcos <curymarcos@live.com>
2022-12-13 20:21:41 -0300

    Modularize code

--- src/main/java/Controller/TomasuloController.java
+++ src/main/java/Controller/TomasuloController.java
@@ -38,37 +38,40 @@ public class TomasuloController {
 
     private static void addInstructionsAndReservationStation() throws Exception {
         for (Instruction instruction : InstructionQueueController.instructionQueue.getAllInstructions()) {
-            boolean hasDependencie = false;
             String registerReadOneName = instruction.getOption2();
             String registerReadTwoName = instruction.getOption3();
-            ReservationStation[] reservationStations = (ReservationStation[]) ReservationStationController.allReservationsArea.values().toArray();
-            for (ReservationStation reservationStation : reservationStations) {
-                ReservationStationInstruction[] reservationStationInstructions = reservationStation.getReservationStationInstructions();
-                for (ReservationStationInstruction reservationStationInstruction : reservationStationInstructions) {
-                    String registerLookingWriteName = reservationStationInstruction.getRegisterTarget().getName();
-                    if (registerLookingWriteName.equals(registerReadOneName) || (registerReadTwoName != null && registerLookingWriteName.equals(registerReadTwoName))) {
-                        hasDependencie = true;
-                        break;
-                    }
-                }
-                if (hasDependencie) {
-                    break;
-                }
-            }
 
-            if (!hasDependencie) {
-                String registerWriteName = instruction.getOption1();
-                TypeEnum instructionReservationStationType = ReservationStationController.findTypeEnumBaseInstruction(instruction.getInstruction());
-                InstructionsEnum instructionType = InstructionController.findInstructionEnumBasedInInstructionName(instruction.getInstruction());
-                ReservationStation selectReservationStation = ReservationStationController.allReservationsArea.get(instructionReservationStationType);
-                selectReservationStation.add(
-                    instruction.getInstruction(), 
-                    instructionType, 
-                    RegisterController.findRegisterBasedInName(registerWriteName), 
-                    RegisterController.findRegisterBasedInName(registerReadOneName), 
-                    RegisterController.findRegisterBasedInName(registerReadTwoName)
-                );
+            if (!verifyIdInstructionHasDependencie(instruction, registerReadOneName, registerReadTwoName)) {
+                addInReservationStation(instruction, registerReadOneName, registerReadTwoName);
             }
         }
+    }
+
+    private static boolean verifyIdInstructionHasDependencie(Instruction instruction, String registerReadOneName, String registerReadTwoName) {
+        ReservationStation[] reservationStations = (ReservationStation[]) ReservationStationController.allReservationsArea.values().toArray();
+        for (ReservationStation reservationStation : reservationStations) {
+            ReservationStationInstruction[] reservationStationInstructions = reservationStation.getReservationStationInstructions();
+            for (ReservationStationInstruction reservationStationInstruction : reservationStationInstructions) {
+                String registerLookingWriteName = reservationStationInstruction.getRegisterTarget().getName();
+                if (registerLookingWriteName.equals(registerReadOneName) || (registerReadTwoName != null && registerLookingWriteName.equals(registerReadTwoName))) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private static void addInReservationStation(Instruction instruction, String registerReadOneName, String registerReadTwoName) throws Exception {
+        String registerWriteName = instruction.getOption1();
+        TypeEnum instructionReservationStationType = ReservationStationController.findTypeEnumBaseInstruction(instruction.getInstruction());
+        InstructionsEnum instructionType = InstructionController.findInstructionEnumBasedInInstructionName(instruction.getInstruction());
+        ReservationStation selectReservationStation = ReservationStationController.allReservationsArea.get(instructionReservationStationType);
+        selectReservationStation.add(
+            instruction.getInstruction(), 
+            instructionType, 
+            RegisterController.findRegisterBasedInName(registerWriteName), 
+            RegisterController.findRegisterBasedInName(registerReadOneName), 
+            RegisterController.findRegisterBasedInName(registerReadTwoName)
+        );
     }
 }
