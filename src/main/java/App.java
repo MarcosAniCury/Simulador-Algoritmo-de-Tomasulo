cbf2563aa029e63058e9738a679f62990791559d
Rayane Paiva <rayanepaiiva@live.com>
2022-12-14 02:13:39 -0300

    up

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,10 +1,10 @@
-import Controller.InstructionQueueController;
+
 import Controller.TomasuloController;
-import Model.Instruction;
-import Model.Tomasulo;
 
 
-import javax.swing.SwingUtilities;
+
+
+
 
 
 public final class App {
@@ -13,17 +13,27 @@ public final class App {
 
         TomasuloController.setupTomasulo("comands.txt");        
 
-        for (Instruction i : InstructionQueueController.getAllInstructions()) {
-            
-            System.out.println(i);
-            
-        }
+        System.out.println("\n");
+        System.out.println("------------------------------------------------------------------------------------------");        
+        System.out.println("\t\t\t\t Reorder Buffer");
+        System.out.println("------------------------------------------------------------------------------------------");        
+        System.out.println("Entry \t Busy \t Instruction \t State \t Destination \t Value");
+        System.out.println("------------------------------------------------------------------------------------------");
+        System.out.println("------------------------------------------------------------------------------------------");
+        System.out.println("\t\t\t\t ReservationStations");
+        System.out.println("------------------------------------------------------------------------------------------");
+        System.out.println("Name \t Busy \t Op \t Vj \t\t Vk \t\t Qj \t Qk \t Dest \t A");
+        System.out.println("------------------------------------------------------------------------------------------");
+        System.out.println("------------------------------------------------------------------------------------------");
+        System.out.println("\t\t\t\t FP register status");
+        System.out.println("------------------------------------------------------------------------------------------");
+        System.out.println("Field \t F0 \t F1 \t F2 \t F3 \t F4 \t F5 \t F6 \t F7 \t F8 \t F9 \t F10");
+        System.out.println("Reorder #");
+        System.out.println("Busy");
+        System.out.println("------------------------------------------------------------------------------------------");
+        System.out.println("------------------------------------------------------------------------------------------");        
+        System.out.println("\n");
 
-
-        SwingUtilities.invokeLater(() -> {
-            Tomasulo t = new Tomasulo();
-            t.setVisible(true);
-        });
 
     }
 }
